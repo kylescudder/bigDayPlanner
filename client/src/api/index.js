4832cd8689c6bbab5b27a276c830db561ef4779a
kylescudder <74150974+kylescudder@users.noreply.github.com>
2021-06-23 20:42:53 +0100

    Added auth-token headers to guest route api calls

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,20 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: `${process.env.REACT_APP_API_URI}/wedding/api`
 })
+export const insertGuest = payload => api.post(`/guest`, payload, {
+    headers: { "x-auth-token": localStorage.getItem("auth-token") }
+})
+export const getAllGuests = () => api.get(`/guest`, {
+    headers: { "x-auth-token": localStorage.getItem("auth-token") }
+})
+export const updateGuestById = (id, payload) => api.put(`/guest/${id}`, payload, {
+    headers: { "x-auth-token": localStorage.getItem("auth-token") }
+})
+export const deleteGuestById = id => api.delete(`/guest/${id}`, {
+    headers: { "x-auth-token": localStorage.getItem("auth-token") }
+})
+export const getGuestById = id => api.get(`/guest/${id}`, {
+    headers: { "x-auth-token": localStorage.getItem("auth-token") }
 })
 
 export const insertGuest = payload => api.post(`/guest`, payload)
