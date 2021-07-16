903630342aedf5c5c86b100450ac61cd9b83ab2b
Kyle Scudder <kyle.s@paloma.co.uk>
2021-07-16 17:35:34 +0100

    Added Guest page routes to router

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Register from './components/auth/Register'
-import Login from './components/auth/Login'
-import Home from './components/pages/Home'
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Home from "./components/pages/Home";
 import GuestList from "./components/pages/GuestList";
+import GuestUpdate from "./components/pages/GuestUpdate";
+import GuestInsert from "./components/pages/GuestInsert";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/layout/Header";
 
@@ -11,14 +13,18 @@ function Routes() {
   return (
     <Router>
       <Header />
+      <div className="container-fluid">
         <Switch>
           <Route exact path="/guest/list" component={GuestList} />
-					<Route exact path="/" component={Home} />
+          <Route exact path="/guest/add" component={GuestInsert} />
+          <Route exact path="/guest/update/:id" component={GuestUpdate} />
+          <Route exact path="/" component={Home} />
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
         </Switch>
+      </div>
     </Router>
   );
-};
+}
 
 export default Routes;
